b3f686c1b829995ab0b8512a320abb2388906765
doyu <ydh0905@nate.com>
2022-03-23 16:11:05 +0900

    CSS: Item Component

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -4,17 +4,21 @@ class Item extends Component {
   render() {
     return (
       <li className="item">
-        <span className="item-name">우유</span>
-        <span className="item-count">0</span>
-        <button className="item-button item-increase">
-          <i class="fas fa-plus-square"></i>
-        </button>
-        <button className="item-button item-decrease">
-          <i class="fas fa-minus-square"></i>
-        </button>
-        <button className="item-button item-delete">
-          <i class="fas fa-trash"></i>
-        </button>
+        <div className="item-info">
+          <span className="item-name">우유</span>
+          <span className="item-count">0</span>
+        </div>
+        <div className="item-button">
+          <button className="item-increase">
+            <i className="fas fa-plus"></i>
+          </button>
+          <button className="item-decrease">
+            <i className="fas fa-minus"></i>
+          </button>
+          <button className="item-delete">
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        </div>
       </li>
     );
   }
